34aacd097cfc1d4169e658dbb6e765fa03a0c0ac
Amit Sarkar <amit.sarkar007@gmail.com>
2020-08-04 13:18:27 +0100

    updated ToDoMVC scripts

--- Taiko-ToDoMVC-Angular.js
+++ Taiko-ToDoMVC-Angular.js
@@ -5,17 +5,17 @@ const assert = require('assert');
 (async () => {
     try {
           //Open Browser and navigate to the page
-          await openBrowser({headless:false});
-          await goto('http://todomvc.com/examples/react/#/'); 
+          await openBrowser({headless:true});
+          await goto('http://todomvc.com/examples/angularjs');
 
-          //Open Browser and navigate to the page
-          await write('Add Task');
+          //'Add Task'
+          await write('Add Task',into(textBox({placeholder:"What needs to be done?"})));
           await press('Enter')
           assert.ok(await text('1 item left').exists());
           await click($("#toggle-all"));
           await click("Clear completed");
 
-          //Open Browser and navigate to the page
+          //'Complete Task'
           await write('Complete Task',into(textBox({placeholder:"What needs to be done?"})));
           await press('Enter')
           assert.ok(await text('1 item left').exists());
@@ -26,7 +26,7 @@ const assert = require('assert');
           await click('Active')
           assert.ok(!(await text('Complete Task').exists()));
 
-          //Open Browser and navigate to the page
+          //'Completed Task'
           await write('Completed Task',into(textBox({placeholder:"What needs to be done?"})));
           await press('Enter')
           assert.ok(await text('1 item left').exists());
@@ -37,18 +37,7 @@ const assert = require('assert');
           await click('Active')
           assert.ok(!(await text('Completed Task').exists()));
 
-          //Open Browser and navigate to the page
-          await write('Complete Task',into(textBox({placeholder:"What needs to be done?"})));
-          await press('Enter')
-          assert.ok(await text('1 item left').exists());
-          await click('Active')
-          assert.ok(await text('Complete Task').exists());
-          await click(checkBox(near('Complete Task')))
-          assert.ok(await text('0 items left').exists());
-          await click('Active')
-          assert.ok(!(await text('Complete Task').exists()));
-
-          //Open Browser and navigate to the page
+          //'Clear Task'
           await write('Clear Task',into(textBox({placeholder:"What needs to be done?"})));
           await press('Enter')
           assert.ok(await text('1 item left').exists());
@@ -57,13 +46,14 @@ const assert = require('assert');
           await click('Clear completed')
           assert.ok(!(await text('Clear Task').exists()));
 
-          //Open Browser and navigate to the page
+          //'Remove Task'
           await write('Remove Task',into(textBox({placeholder:"What needs to be done?"})));
           await press('Enter')
           assert.ok(await text('1 item left').exists());
           await hover("Remove Task");
           await click(button({class:"destroy"}));
           assert.ok(!(await text('Remove Task').exists()));
+
     } catch (error) {
         console.error(error);
     } finally {
---
+++ Taiko-ToDoMVC-Vue.js
@@ -0,0 +1,62 @@
+//SOURCE - https://github.com/getgauge-contrib/compareBrowserAutomationTools/blob/master/compareCostOfSelectorMaintenance/taiko/tests/activities.js
+
+const { openBrowser,closeBrowser, goto, checkBox, click, near,into,$,textBox,hover,button, write, text, press } = require('taiko');
+const assert = require('assert');
+(async () => {
+    try {
+          //Open Browser and navigate to the page
+          await openBrowser({headless:true});
+          await goto('http://todomvc.com/examples/vue');
+
+          //'Add Task'
+          await write('Add Task',into(textBox({placeholder:"What needs to be done?"})));
+          await press('Enter')
+          assert.ok(await text('1 item left').exists());
+          await click($("#toggle-all"));
+          await click("Clear completed");
+
+          //'Complete Task'
+          await write('Complete Task',into(textBox({placeholder:"What needs to be done?"})));
+          await press('Enter')
+          assert.ok(await text('1 item left').exists());
+          await click('Active')
+          assert.ok(await text('Complete Task').exists());
+          await click(checkBox(near('Complete Task')))
+          assert.ok(await text('0 items left').exists());
+          await click('Active')
+          assert.ok(!(await text('Complete Task').exists()));
+
+          //'Completed Task'
+          await write('Completed Task',into(textBox({placeholder:"What needs to be done?"})));
+          await press('Enter')
+          assert.ok(await text('1 item left').exists());
+          await click('Active')
+          assert.ok(await text('Completed Task').exists());
+          await click(checkBox(near('Completed Task')))
+          assert.ok(await text('0 items left').exists());
+          await click('Active')
+          assert.ok(!(await text('Completed Task').exists()));
+
+          //'Clear Task'
+          await write('Clear Task',into(textBox({placeholder:"What needs to be done?"})));
+          await press('Enter')
+          assert.ok(await text('1 item left').exists());
+          await click(checkBox(near('Clear Task')))
+          assert.ok(await text('0 items left').exists());
+          await click('Clear completed')
+          assert.ok(!(await text('Clear Task').exists()));
+
+          //'Remove Task'
+          await write('Remove Task',into(textBox({placeholder:"What needs to be done?"})));
+          await press('Enter')
+          assert.ok(await text('1 item left').exists());
+          await hover("Remove Task");
+          await click(button({class:"destroy"}));
+          assert.ok(!(await text('Remove Task').exists()));
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await closeBrowser();
+    }
+})();
