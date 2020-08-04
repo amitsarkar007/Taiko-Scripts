c51861252a58de791fe112ca42fe4a979c310fed
Amit Sarkar <amit.sarkar007@gmail.com>
2020-08-04 16:25:16 +0100

    updated Google Flights script

--- Taiko-GoogleFlights.js
+++ Taiko-GoogleFlights.js
@@ -1,9 +1,9 @@
 //Script still in progress.
 
-const { openBrowser, goto, click, $, closeBrowser, into, textBox, write, screenshot, press, evaluate, waitFor, dropDown } = require('taiko');
+const { openBrowser, goto, click, $, closeBrowser, into, textBox, write, screenshot, press, evaluate, waitFor, dropDown, text, highlight } = require('taiko');
 const origin_airport = "Berlin";
 const destination_airport = "Madrid";
-const departure_date = "01 Aug";
+const departure_date = "20 Aug";
 const return_date = "31 Aug" ;
 
 (async () => {
@@ -12,46 +12,43 @@ const return_date = "31 Aug" ;
         await openBrowser({ headless: false ,args:['--window-size=1440,1200']});
         await goto("https://www.google.co.uk/flights");
 
-        //Enter origin and destination airport
-        await waitFor( async () => await $(`[data-flt-ve=origin_airport]`).isVisible());
-        await click($(`[data-flt-ve=origin_airport]`));
-        await write(origin_airport, into(textBox({placeholder:"Where from?"})));
+        //Enter origin airport
+        await write(origin_airport,into(textBox(below("Round trip"))));
         await click("All airports");
-        await waitFor( async () => await $(`[data-flt-ve=destination_airport]`).isVisible());
-        await press("Tab");
 
-        //await evaluate($(`[data-flt-ve=destination_airport]`),(elem) => elem.click());
-        await write(destination_airport);//, into(textBox({placeholder:"Where to?"})));
+        //Enter destination airport
+        await write(destination_airport,into(textBox(below("Economy"))))
         await click("All airports");
         await press("Tab");
 
-        //Enter start and end date of journey
-        //await click($(`.gws-flights-form__date-content`));
-        await write(departure_date);//, into(textBox({placeholder:"Departure date"})));
+        //Enter start date of journey
+        await write(departure_date);
         await press("Enter");
+
+        //Enter end date of journey
         await write(return_date, into(textBox({placeholder:"Return date"})));
         await press("Enter");
         await click("Done");
 
         //Select bags
-        await click("Bags");
-        await click($(`[aria-label=Increase]`));
-        await press("Escape");
+        await evaluate($(`[data-chip=bags]`),(elem) => elem.click());
+        await evaluate($(`[aria-label=Increase]`),(elem) => elem.click());
+        await click($(`[aria-label=Close]`));
 
         //Select stops
-        await click("Stops");
-        await click("No stops only");
-        await press("Escape");
+        await evaluate($(`[data-chip=stops]`),(elem) => elem.click());
+        await evaluate($(`//*[contains(text(),'No stops only')]`),(elem) => elem.click());
+        await click($(`[aria-label=Close]`));
 
         //Select price
-        await click("Price");
-        await range($(`.gws-flights-filter__price-slider`)).select('400');
-        await press("Escape");
+        await evaluate($(`[data-chip=price]`),(elem) => elem.click());
+        await range($(`.gws-flights-filter__price-slider`)).select('400'); //Incremental values of 200
+        await click($(`[aria-label=Close]`));
 
         //Select times
-        await click("Times");
-        await click("No stops only");
-        await press("Escape");
+        await evaluate($(`[data-chip=times]`),(elem) => elem.click());
+        //await click("No stops only");
+        //await click($(`[aria-label=Close]`));
 
         //Take screenshot of all flight options
         await screenshot({fullPage:true});
