d3679ab8ae128647c6f0f99dab0ecd0a71d2456c
Amit Sarkar <amit.sarkar007@gmail.com>
2021-02-25 18:29:04 +0000

    new Danube script added

---
+++ Taiko - Danube.js
@@ -0,0 +1,21 @@
+const { openBrowser, goto, click, closeBrowser, textBox } = require('taiko');
+(async () => {
+    try {
+        // Open Browser and navigate to the page
+        await openBrowser({ headless: false, args:['--window-size=1440,1000']});
+        await goto('https://danube-webshop.herokuapp.com/');
+
+        // Accept Cookie alert
+        await click("Log In");
+
+        // Enter user credentials
+        await write("user@email.com",into(textBox('Email')));
+        await write("supersecure1",into(textBox('Password')));
+        await click("Sign in");
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await closeBrowser();
+    }
+})();
